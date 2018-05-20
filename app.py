e5bd767c754585a2264546a2769a737247c5e601
Paul <kontakt@pawelkot.com.pl>
2018-05-20 19:27:06 +0200

    Add Save to txt class

--- app.py
+++ app.py
@@ -11,24 +11,34 @@ app = Flask(__name__)
 def index():
 	form = DomainForm(request.form)
 	parser = Parser()
+	file = File()
 	url = None
+	save = None
 	if request.method == 'POST' and form.validate():
 		parser.connect(form.domain.data)
-		url = parser.pars()
+		url = parser.pars_url()
+		save = file.save(url)
 	return render_template('index.html', form=form, result=url)
-
 
 class Parser:
 	def connect(self, url):
 		self.url = url
 		self.rq = rq.urlopen(self.url)
 
-	def pars(self):
+	def pars_url(self):
 		self.p = bs(self.rq, "html.parser")
 		self.r = []
 		for k in self.p.find_all('a'):
 			self.r.append(k.get('href'))
 		return self.r
+
+class File:
+	def save(self, url):
+		self.f = open('links.txt', 'w')
+		for k in url:
+			self.f.write(k+'\n')
+		self.f.close()
+
 
 
 if __name__ == '__main__':
